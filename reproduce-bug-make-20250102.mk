31e8bd04ed2b5baf7e7272ef83908e98f7c247a7
Dimitar Dimitrov <mail.mitko@gmail.com>
2025-01-02 20:11:08 +0100

    Update Makefile (potential) bug test
    Upload an artifact with the results

--- reproduce-bug-make-20250102.mk
+++ reproduce-bug-make-20250102.mk
@@ -1,26 +1,39 @@
-FOO := first
+FOO := v1
 BAR = $(FOO)
 $(info [1] BAR: $(BAR)) # OK
-FOO := second
+FOO := v2
 $(info [2] BAR: $(BAR)) # OK
-FOO += third
+FOO += v3
 $(info [3] BAR: $(BAR)) # OK
 
-all: potential-bug info
-
-# peculiar behaviour when the target-local FOO is defined using:
-#  1. FOO := fourth
-#  2. FOO += fourth
-#  3. override FOO += fourth (when we make x FOO=something)
-#  4. ...
-potential-bug: override FOO += fourth
-potential-bug:
+with-target-specific-variable: override FOO += v41
+with-target-specific-variable:
+	$(info ------ with-target-specific-variable ------ )
 	@echo "[4] BAR: $(BAR)" # OK
-	@$(eval FOO += fifth)
+	@$(eval FOO += v51)
 	@echo "[5] BAR: $(BAR)" # not what I expect to see
-	@$(eval FOO += sixth)
+	@$(eval FOO := v61)
 	@echo "[6] BAR: $(BAR)" # not what I expect to see
 
+with-target-specific-override-variable: override FOO += v41
+with-target-specific-override-variable:
+	$(info ------ with-target-specific-override-variable ------ )
+	@echo "[4] BAR: $(BAR)" # OK
+	@$(eval FOO += v51)
+	@echo "[5] BAR: $(BAR)" # not what I expect to see
+	@$(eval FOO := v61)
+	@echo "[6] BAR: $(BAR)" # not what I expect to see
+
+without-target-specific-variable:
+without-target-specific-variable:
+	$(info ------ without-target-specific-variable ------ )
+	@echo "[7] BAR: $(BAR)" # OK
+	@$(eval FOO += v52)
+	@echo "[8] BAR: $(BAR)" # OK
+	@$(eval FOO := v62)
+	@echo "[9] BAR: $(BAR)" # OK
+
 info:
-	$(MAKE) --version
-	$(MAKE) --help | tail -2 | head -1
+	$(info ------ info ------ )
+	@$(MAKE) --help | tail -2 | head -1
+	@$(MAKE) --version
